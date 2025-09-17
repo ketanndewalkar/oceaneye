716a74d2a48e51593ad549dfc7aac4adc24b16fe
Ketan <dewalkarketan123@gmail.com>
2025-09-17 19:54:16 +0530

    Added ReportIncident.jsx

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -79,6 +79,7 @@ export const login = asyncHandler(async (req, res) => {
   const cookieOptions = {
     httpOnly: true,
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+    
   };
 
   return res
