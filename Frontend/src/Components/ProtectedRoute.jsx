b3a706bb5756feb787620575841828ef68e89097
Ketan <dewalkarketan123@gmail.com>
2025-09-20 16:23:23 +0530

    fixed navbar login

--- Frontend/src/Components/ProtectedRoute.jsx
+++ Frontend/src/Components/ProtectedRoute.jsx
@@ -1,12 +1,15 @@
 // ProtectedRoute.jsx
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { AuthContext } from "../Context/AuthContext";
 import { useContext } from "react";
 
 
 const ProtectedRoute = ({ children }) => {
   const { user } = useContext(AuthContext);
-
+  const location = useLocation();
+  if(location.pathname=="/"){
+    return children;
+  }
   if (!user) {
     return <Navigate to="/login" replace />;
   }
