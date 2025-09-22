7b4a0c00fa67c2c7cfab3f5771249737a283d0d8
Ketan <dewalkarketan123@gmail.com>
2025-09-22 15:38:08 +0530

    second commit

--- Backend/index.js
+++ Backend/index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: process.env.FRONTEND_URL || "http://localhost:4000", 
     credentials: true,
   })
 );
